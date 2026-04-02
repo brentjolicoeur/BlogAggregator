bb9cf09172547a2138a30e387b14ea32ac2071b6
brentjolicoeur <brentjolicoeur@gmail.com>
2026-04-02 08:24:00 -0400

    internal directory structure and config struct set

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brentjolicoeur/BlogAggregator
+
+go 1.26.1
---
+++ internal/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() {
+	return
+}
