a8f5452c78d0eea5256a9531a484a0b8291c1c0e
brentjolicoeur <brentjolicoeur@gmail.com>
2026-04-05 12:14:37 -0400

    agg command working (3.1 complete)

--- command_handlers.go
+++ command_handlers.go
@@ -84,3 +84,15 @@ func handlerUsers(s *state, cmd command) error {
 	}
 	return nil
 }
+
+func handlerAgg(s *state, cmd command) error {
+	feedURL := "https://www.wagslane.dev/index.xml"
+
+	feed, err := fetchFeed(context.Background(), feedURL)
+	if err != nil {
+		return fmt.Errorf("Error fetching feed: %v\n", err)
+	}
+	fmt.Println("Feed fectched successfully.")
+	fmt.Printf("%+v\n", feed)
+	return nil
+}
