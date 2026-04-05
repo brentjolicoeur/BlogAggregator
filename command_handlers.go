a869b32275f769d79f19dffbf42a4b569bb089d7
brentjolicoeur <brentjolicoeur@gmail.com>
2026-04-05 13:33:02 -0400

    addfeeds command working (3.2 complete)

--- command_handlers.go
+++ command_handlers.go
@@ -96,3 +96,30 @@ func handlerAgg(s *state, cmd command) error {
 	fmt.Printf("%+v\n", feed)
 	return nil
 }
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.args) < 2 {
+		return errors.New("command syntax: addfeed <Name> <url>")
+	}
+	name := cmd.args[0]
+	feedURL := cmd.args[1]
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("Error retrieving user: %v\n", err)
+	}
+	feedParams := database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+		Url:       feedURL,
+		UserID:    user.ID,
+	}
+	feed, err := s.db.CreateFeed(context.Background(), feedParams)
+	if err != nil {
+		return fmt.Errorf("Error creating feed: %v\n", err)
+	}
+	fmt.Println("Feed created successfully")
+	fmt.Printf("%+v\n", feed)
+	return nil
+}
